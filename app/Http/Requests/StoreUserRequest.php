c437c411d83e7c346497f20aaef6ba45cd04049e
YanFranko-GS <gonzalessegurayanfranko@gmail.com>
2026-05-23 00:56:53 -0500

    pequeñas mejoras implementadas

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -12,7 +12,7 @@ class StoreUserRequest extends FormRequest
     public function authorize(): bool
     {
         return true;
-    }
+    } 
 
     /**
      * Get the validation rules that apply to the request.
